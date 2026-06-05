951de60aadef86a32e66d4971356e7725463ff14
alex-zander-0403 <alexzander0403@gmail.com>
2026-06-05 22:51:55 +0300

    mongo atlas connected

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+//
+const app = express();
+const port = process.env.PORT || 8001;
+const mongoURI = process.env.MONGO_DB_URI;
+
+// создаем екземпляр клиента для работы mongoBD
+const myMongoClient = new MongoClient(mongoURI);
+
+// endpoints
+app.get("/", (req, res) => {
+  res.send("hello express!");
+});
+
+// подключение к бд
+async function runDB() {
+  try {
+    // подключение
+    await myMongoClient.connect();
+
+    // пингуем
+    // await myMongoClient.db("users").command({ ping: 1 });
+    console.log("✅ База данных готова к работе!");
+  } catch (error) {
+    console.log("❌ Не могу подключиться к базе данных!");
+    console.log("Ошибка:", error.message); // причина
+    await myMongoClient.close();
+  }
+}
+
+runDB();
