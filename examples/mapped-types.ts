e408059a61ff5cfe1421fc58bf9c93749f1512d2
alex-zander-0403 <alexzander0403@gmail.com>
2026-06-09 02:07:10 +0300

    mapped types

---
+++ examples/mapped-types.ts
@@ -0,0 +1,45 @@
+// base type
+interface User {
+  id: number;
+  login: string;
+}
+
+// ===============
+
+// тип утилита, который создает новый тип с уже readonly свойствами
+type ReadOnlyType<T> = {
+  readonly [Key in keyof T]: T[Key];
+  // -readonly [Key in keyof T]: T[Key]; // обратный эффект
+};
+
+type newUser1 = ReadOnlyType<User>;
+// {
+//   readonly id: number;
+//   readonly login: string;
+// }
+
+// ===============
+
+// тип утилита, который создает новый тип с уже опциональными свойствами! (-)
+type OptionalType<T> = {
+  [Key in keyof T]?: T[Key];
+  // [Key in keyof T]-?: T[Key]; // обратный эффект
+};
+
+type newUser2 = OptionalType<User>;
+// {
+//   id?: number | undefined;
+//   login?: string | undefined;
+// }
+
+// ===============
+
+// тип утилита, который создает новый тип с исключенными свойствами!
+type WithoutId<T> = {
+  [Key in keyof T as Exclude<Key, "id">]: T[Key];
+};
+
+type newUser3 = WithoutId<User>;
+// {
+//   login: string;
+// }
